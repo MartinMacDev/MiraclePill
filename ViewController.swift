c94ce41cad56d26d729c2339334bdd0da1293d07
Martinus Schempers <martinus@Martinuss-Mac.local>
2016-12-31 11:37:57 -0800

    FirstCommit

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Martinus Schempers on 2016/12/31.
+//  Copyright © 2016 MartinMacDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
