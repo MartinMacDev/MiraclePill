0e4ce8dc807e9270ca2258ab0a9f1cc69d18ef31
Martinus Schempers <martinus@Martinuss-Mac.local>
2017-01-02 08:56:08 -0800

    Updated 1

--- ViewController.swift
+++ ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buyNowBtn: UIButton!
+    @IBOutlet weak var pillImage: UIImageView!
+    @IBOutlet weak var miraclePill: UILabel!
+    @IBOutlet weak var dollarOneFifty: UILabel!
+    @IBOutlet weak var dividerBar: UIView!
+    @IBOutlet weak var fullName: UILabel!
+    @IBOutlet weak var enterName: UITextField!
+    @IBOutlet weak var streetAddress: UILabel!
+    @IBOutlet weak var enterAddress: UITextField!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var enterCity: UITextField!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var enterState: UITextField!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var enterCountry: UITextField!
+    @IBOutlet weak var zipCode: UILabel!
+    @IBOutlet weak var enterZip: UITextField!
+    @IBOutlet weak var successImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,6 +39,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buyNow(_ sender: Any) {
+        buyNowBtn.isHidden = true
+        pillImage.isHidden = true
+        miraclePill.isHidden = true
+        dollarOneFifty.isHidden = true
+        dividerBar.isHidden = true
+        fullName.isHidden = true
+        enterName.isHidden = true
+        streetAddress.isHidden = true
+        enterAddress.isHidden = true
+        cityLabel.isHidden = true
+        enterCity.isHidden = true
+        stateLabel.isHidden = true
+        enterState.isHidden = true
+        countryLabel.isHidden = true
+        enterCountry.isHidden = true
+        zipCode.isHidden = true
+        enterZip.isHidden = true
+        successImage.isHidden = false
+    }
 
 }
 
